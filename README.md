19040cab3cee53b7fa7311287391463ca4db3dd8
DS-1090 <divya.yennam5544@gmail.com>
2024-12-26 23:42:40 +0530

    add files

---
+++ README.md
@@ -0,0 +1,13 @@
+# HydHomePrice_Forecast
+A Random Forest-based ML regression model that predicts the price of a home in Hyderabad based on Location and Sqft. 
+Deployed on the web using Flask 
+Trained on 2021 data
+Has r2 score of ~0.70
+Run hyd_mega.ipynb for training the model
+
+Tech Stack: Flask, HTML, CSS, Numpy, Pandas, MatplotLib, Scikit-Learn, Pickle, TensorFlow, Keras
+
+Also has an ANN model with r2 score of ~0.465.
+
+![Screenshot 2024-05-18 185251](https://github.com/DS-1090/HydHomePrice_Forecast/assets/126580400/c7f33bca-edef-4718-bbaa-16eba71116c8)
+![Screenshot 2024-05-18 185212](https://github.com/DS-1090/HydHomePrice_Forecast/assets/126580400/7bd0b566-439f-4189-b46e-5cb3b595b0dd)
---
+++ home.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+
+st.header("Hyderabad Home Forecast")
+st.caption('Estimate the price of a home based on Locality and Sqft (2021 data)')
+st.image('bg.png', use_container_width=True)
+
+model = pickle.load(open('D:/hyd_home_price_predict/vercel_home_pred/forest_model.pkl', 'rb'))
+
+areas = [
+    "Alkapur Township, Manikonda, Outer Ring Road",
+    "Aminpur",
+    "Attapur",
+    "Bachupally",
+    "Bandlaguda Jagir",
+    "Beeramguda, Ramachandra Puram, NH 9",
+    "Boduppal, NH 22",
+    "Chandanagar, NH 9",
+    "Dammaiguda",
+    "Gachibowli",
+    "Gajularamaram",
+    "Hafeezpet, NH 9",
+    "Kokapet, Outer Ring Road",
+    "Kollur, Outer Ring Road",
+    "Kompally",
+    "Kondapur",
+    "Kukatpally, NH 9",
+    "LB Nagar, NH 9",
+    "Madhapur",
+    "Madinaguda, Hafeezpet, NH 9",
+    "Mallampet, Outer Ring Road",
+    "Manikonda, Outer Ring Road",
+    "Miyapur, NH 9",
+    "Nagaram",
+    "Nagole",
+    "Nallagandla, Serilingampally",
+    "Narsingi, Outer Ring Road",
+    "Nizampet",
+    "Patancheru",
+    "Pragathi Nagar, Kukatpally",
+    "Puppalaguda",
+    "Rajendra Nagar, Outer Ring Road",
+    "Shamirpet",
+    "Tellapur, Outer Ring Road",
+    "Toli Chowki",
+    "Uppal, NH 22",
+]
+
+st.caption('This is a Random Forest based ML model having r-squared score of ~0.70.')
+
+area = st.selectbox("Select Locality:", areas)
+area_index = areas.index(area)
+
+sqft = st.slider('Select SqFt Area:', min_value=200, max_value=3500, step=50)
+
+if st.button("Submit", type='primary'):
+    if sqft > 0:
+        feature_names = ['Locality','Sqft']
+        input_arr = pd.DataFrame([[ area_index, sqft]], columns=feature_names)
+
+        prediction = model.predict(input_arr)
+
+        output = round(prediction[0], 2)
+        
+        st.success(f'The predicted price of the house is {output} Lakhs')
+        st.balloons()
+        sqft=0
+        
+    else:
+        st.error("Please select a valid square footage.")
+
+
